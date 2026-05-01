ba121857948e377c252498089a0750dc88ab2e36
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2026-05-01 01:37:10 -0600

    add dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Dashboard from "./dashboard/page";
 
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-     
+      <Dashboard/>
     </div>
   );
 }
